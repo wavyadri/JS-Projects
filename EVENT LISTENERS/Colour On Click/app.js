aa7f4cde08c413acbbe1ef4a3eb5f6a532104e49
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-04-26 13:46:09 -0400

    Complete

---
+++ EVENT LISTENERS/Colour On Click/app.js
@@ -0,0 +1,17 @@
+const square1 = document.querySelector('.square-1');
+const square2 = document.querySelector('.square-2');
+const square3 = document.querySelector('.square-3');
+const square4 = document.querySelector('.square-4');
+
+square1.addEventListener('click', function() {
+    square1.classList.toggle("red");
+})
+square2.addEventListener('click', function() {
+    square2.classList.toggle("green");
+})
+square3.addEventListener('click', function() {
+    square3.classList.toggle("blue");
+})
+square4.addEventListener('click', function() {
+    square4.classList.toggle("yellow");
+})
