04e21b13e54090ff328076197455f603716c5c5a
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-04-27 18:54:21 -0400

    Tarot Progress

--- APIs/Tarot Reading/app.js
+++ APIs/Tarot Reading/app.js
@@ -1,11 +1,67 @@
 const begin = document.querySelector('.btn');
-const spinner = document.querySelectorAll('#js-spinner');
 
-begin.addEventListener('click', pickCards);
+// begin.addEventListener('click', pickCards);
 
 const endpoint = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3";
 
-function pickCards() {
-    spinner.classList.remove('hidden');
-    begin.disabled = true;
+// async function pickCards() {
+//     begin.disabled = true;
+
+//     try {
+//         const response = await fetch(endpoint);
+//         if (!response.ok) {
+//             throw Error(response.statusText);
+//         }
+//     }
+// }
+
+fetch(endpoint)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    pastCard(data)
+    // presentCard(data)
+    // futureCard(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+function pastCard(data) {
+    const pastTitle = data.cards[0];
+    const pastDiv = document.getElementById('past');
+
+    // Name
+    const pastName = pastTitle.name;
+    const pastHeading = document.createElement("h2");
+    pastHeading.innerHTML = pastName;
+    pastDiv.appendChild(pastHeading);
+
+    // Card meaning
+    const pastMeaningUp = pastTitle['meaning_up'];
+    const pastBody = document.createElement("p");
+    pastBody.innerHTML = pastMeaningUp;
+    pastDiv.appendChild(pastBody);
+
+    // Arcana    
+    const pastType = pastTitle.type;
+    const pastBodyType = document.createElement("p");
+    pastBodyType.innerHTML = pastType;
+    pastDiv.appendChild(pastBodyType);
+
+    // Suit
+    const pastSuit = pastTitle.suit;
+    const pastBodySuit = document.createElement("p");
+    pastBodySuit.innerHTML = pastSuit;
+    pastDiv.appendChild(pastBodySuit);
+
+    // Value
+    const pastValue = pastTitle.value;
+    const pastBodyValue = document.createElement("p");
+    pastBodyValue.innerHTML = pastValue;
+    pastDiv.appendChild(pastBodyValue);
 }
