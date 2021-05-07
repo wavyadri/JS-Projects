16f404acff71643c8c9358cb6cad933b37e92c54
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-05-07 09:56:38 -0400

    Initial Commit
    Beginning new project.

---
+++ LISTS/Shop List 2.0/app.js
@@ -0,0 +1,6 @@
+// type in input, submit or click event to output HTML
+// output has CHECK and X
+// CHECK click event strikes out item
+// X click event deletes item
+// item counter: item add = +1. item deleted OR completed = -1.
+// when only one item left unchecked, trigger a reminder
