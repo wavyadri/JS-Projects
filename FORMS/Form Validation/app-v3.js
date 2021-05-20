28a80c923a0f0f4f8834dafe934effcb599b74a4
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-05-20 09:49:24 -0400

    Update style.scss

--- FORMS/Form Validation/app-v3.js
+++ FORMS/Form Validation/app-v3.js
@@ -5,12 +5,14 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 const termsYes = document.querySelector('#terms-yes');
 const termsNo = document.querySelector('#terms-no');
+const submit = document.querySelector('.submit');
 
 let currentErr = [];
 
 validateForm = (e) => {
     e.preventDefault();
 
+    // clear all errors
     removeErr(currentErr);
 
     currentErr = [];
@@ -21,78 +23,86 @@ validateForm = (e) => {
 
     // pronouns error
     if (pronouns.value == 'none') {
-        currentErr.push({id: 'pronouns', message: 'Error! Select a pronoun!'});
+        currentErr.push({id: 'pronouns', message: 'Please select an option.'});
     }
 
     // name error
     if (firstName.value == '' || (!nameReg.test(firstName.value))) {
-        currentErr.push({id: 'first-name', message: 'Error! Incorrect name format'});
+        currentErr.push({id: 'first-name', message: 'Please enter your first name. No numbers or special characters.'});
     }
 
     // email error
     if (email.value == '' || (!emailReg.test(email.value))) {
-        currentErr.push({id: 'email', message: 'Error. Please enter a valid email'});
+        currentErr.push({id: 'email', message: 'Please enter a valid email.'});
     }
 
     // password error
     if (password.value == '' || (!passReg.test(password.value))) {
-        currentErr.push({id: 'password', message: 'Error. Invalid password.'});
+        currentErr.push({id: 'password', message: 'Please enter a valid password. Minimum eight characters, at least one upper case letter, one lower case letter, one number and one special character'});
     }
 
     // confirm password error
     if (confirmPassword.value == '' || (confirmPassword.value !== password.value)) {
-        currentErr.push({id: 'confirm-password', message: 'Error. Password do not match'});
+        currentErr.push({id: 'confirm-password', message: 'Please try again. Passwords do not match.'});
     }
 
     // privacy error
     if ((!termsYes.checked && !termsNo.checked) || termsNo.checked) {
-        currentErr.push({id: 'privacy', message: 'Error. Please agree to the terms.'})
+        currentErr.push({id: 'privacy', message: 'Please read and agree to the terms.'});
     }
 
-
-    // only want to return false if one or more cases are invalid
-    // cycle through errors[i].error
-    // if any are true
-    // return false (aka do not submit form)
     if (currentErr.length > 0) {
-        displayErr(currentErr)
+        displayErr(currentErr);
+        countErr(currentErr);
         return false;
     } else {
         const form = document.querySelector('#register');
         form.reset();
-        alert('form submitted')
+        alert('Form submitted. Thank you!')
         return true;
     }
 
 }
 
 displayErr = (errs) => {
+    // create message for all current errors
     errs.forEach( (error) => {
         const newEle = document.querySelector(`.${error.id}`);
-        // newEle.removeChild(newEle.lastElementChild);
         const node = document.createElement('p');
-        node.setAttribute('class', `${error.id}-err`); /// or this could just be ('class', 'err') if they can all be styled the same
+        node.setAttribute('class', 'err');
         node.innerHTML = `${error.message}`;
         newEle.append(node);
     })
 }
 
 removeErr = (errs) => {
+    // remove errors
     errs.forEach((error) => {
         const element = document.querySelector(`.${error.id}`);
         if(typeof(element) != 'undefined' && element != null){
             element.innerHTML = '';
         }
     })
+
+    // remove submit error
+    if(typeof(submit) != 'undefined' && submit != null){
+        submit.innerHTML = '';
+    }
 }
 
-clearForm = () => {
-    pronouns.value = 'none';
-    firstName.value = '';
-    email.value = '';
-    password.value = '';
-    confirmPassword.value = '';
-    termsYes.reset();
+countErr = (errs) => {
+    const str = errs.map((error) => error.id).join(', ');
+
+    // submit error message with error count
+    const nodeSubmit = document.createElement('p');
+    nodeSubmit.setAttribute('class', 'submit-err');
+    if (errs.length == 1) {
+        nodeSubmit.innerHTML = `Please fix 1 error to submit: ${str}`;
+        submit.append(nodeSubmit);
+    } else if (errs.length > 1) {
+        nodeSubmit.innerHTML = `Please fix ${errs.length} errors to submit: ${str}`;
+        submit.append(nodeSubmit);
+    }
 }
 
 const submitButton = document.querySelector("#submit");
